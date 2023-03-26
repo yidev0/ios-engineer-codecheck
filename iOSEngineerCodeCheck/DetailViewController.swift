b2b206ee57019e1314d9b815f0d1260f59f6ff70
Yuto Iwakami <11906960+yidev0@users.noreply.github.com>
2023-03-26 15:09:58 +0900

    命名規約の更新

---
+++ iOSEngineerCodeCheck/DetailViewController.swift
@@ -0,0 +1,59 @@
+//
+//  DetailViewController.swift
+//  iOSEngineerCodeCheck
+//
+//  Created by 史 翔新 on 2020/04/21.
+//  Copyright © 2020 YUMEMI Inc. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    @IBOutlet weak var starLabel: UILabel!
+    @IBOutlet weak var watcherLabel: UILabel!
+    @IBOutlet weak var forksLabel: UILabel!
+    @IBOutlet weak var issuesLabel: UILabel!
+    
+    var repoView: RepositoriesViewController!
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let repo = repoView.repositories[repoView.selectedRow]
+        
+        languageLabel.text = "Written in \(repo["language"] as? String ?? "")"
+        starLabel.text = "\(repo["stargazers_count"] as? Int ?? 0) stars"
+        watcherLabel.text = "\(repo["wachers_count"] as? Int ?? 0) watchers"
+        forksLabel.text = "\(repo["forks_count"] as? Int ?? 0) forks"
+        issuesLabel.text = "\(repo["open_issues_count"] as? Int ?? 0) open issues"
+        getImage()
+        
+    }
+    
+    func getImage(){
+        
+        let repo = repoView.repositories[repoView.selectedRow]
+        
+        titleLabel.text = repo["full_name"] as? String
+        
+        if let owner = repo["owner"] as? [String: Any] {
+            if let imgURL = owner["avatar_url"] as? String {
+                URLSession.shared.dataTask(with: URL(string: imgURL)!) { (data, res, err) in
+                    let img = UIImage(data: data!)!
+                    DispatchQueue.main.async {
+                        self.imageView.image = img
+                    }
+                }.resume()
+            }
+        }
+        
+    }
+    
+}
--- iOSEngineerCodeCheck/RepositoriesViewController.swift
+++ iOSEngineerCodeCheck/RepositoriesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RepositoriesViewController.swift
 //  iOSEngineerCodeCheck
 //
 //  Created by 史 翔新 on 2020/04/20.
@@ -8,22 +8,22 @@
 
 import UIKit
 
-class ViewController: UITableViewController, UISearchBarDelegate {
+class RepositoriesViewController: UITableViewController, UISearchBarDelegate {
 
-    @IBOutlet weak var SchBr: UISearchBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    var repo: [[String: Any]]=[]
+    var repositories: [[String: Any]]=[]
     
-    var task: URLSessionTask?
-    var word: String!
+    var urlTask: URLSessionTask?
+    var searchText: String!
     var url: String!
-    var idx: Int!
+    var selectedRow: Int!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        SchBr.text = "GitHubのリポジトリを検索できるよー"
-        SchBr.delegate = self
+        searchBar.text = "GitHubのリポジトリを検索できるよー"
+        searchBar.delegate = self
     }
     
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
@@ -33,19 +33,19 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        task?.cancel()
+        urlTask?.cancel()
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         
-        word = searchBar.text!
+        searchText = searchBar.text!
         
-        if word.count != 0 {
-            url = "https://api.github.com/search/repositories?q=\(word!)"
-            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+        if searchText.count != 0 {
+            url = "https://api.github.com/search/repositories?q=\(searchText!)"
+            urlTask = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
                 if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
                     if let items = obj["items"] as? [[String: Any]] {
-                    self.repo = items
+                    self.repositories = items
                         DispatchQueue.main.async {
                             self.tableView.reloadData()
                         }
@@ -53,7 +53,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
                 }
             }
         // これ呼ばなきゃリストが更新されません
-        task?.resume()
+        urlTask?.resume()
         }
         
     }
@@ -61,22 +61,22 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         if segue.identifier == "Detail"{
-            let dtl = segue.destination as! ViewController2
-            dtl.vc1 = self
+            let detailView = segue.destination as! DetailViewController
+            detailView.repoView = self
         }
         
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return repo.count
+        return repositories.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell()
-        let rp = repo[indexPath.row]
-        cell.textLabel?.text = rp["full_name"] as? String ?? ""
-        cell.detailTextLabel?.text = rp["language"] as? String ?? ""
+        let repo = repositories[indexPath.row]
+        cell.textLabel?.text = repo["full_name"] as? String ?? ""
+        cell.detailTextLabel?.text = repo["language"] as? String ?? ""
         cell.tag = indexPath.row
         return cell
         
@@ -84,7 +84,7 @@ class ViewController: UITableViewController, UISearchBarDelegate {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 画面遷移時に呼ばれる
-        idx = indexPath.row
+        selectedRow = indexPath.row
         performSegue(withIdentifier: "Detail", sender: self)
         
     }
